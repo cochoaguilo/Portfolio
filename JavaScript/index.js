283cfab2b92a0ccb9cc33301edde0bc362968348
Marcos Aguiló <marcosaguilo@MacBook-Pro-de-Marcos.local>
2021-02-11 18:42:05 -0300

    primer commit del portfolio web

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".iconos").mouseenter(function(){
+        $(this).fadeOut(3000);
+       $("p").fadeIn(3000);
+    })
+    //$(".iconos").mouseleave(function(){
+      //  $("p").addClass("hidden");
+     //})
+})
